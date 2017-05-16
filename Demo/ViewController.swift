5673e53bde816019158b8a8d60c2c813a248c1b8
Shabir Jan <iam.shabir@yahoo.com>
2017-05-16 22:05:35 +0500

    Initial commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Shabir Jan on 5/14/17.
+//  Copyright © 2017 Shabir Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("shabir")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+}
+
